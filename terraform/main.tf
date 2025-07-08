adcdcd9a9e3de25a02a7243544164ee5eea4421a
Daniel1magdy <danielmagdy45@gmail.com>
2025-07-08 11:19:37 +0000

    8 Super Mario EKS CI/CD setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,8 @@ module "vpc" {
   azs             = ["eu-west-2a", "eu-west-2b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
+  map_public_ip_on_launch = true
+
   enable_nat_gateway = false
   single_nat_gateway = false
 
